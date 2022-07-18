6a458734cfac4de81ae574dbacfb26e8d24b8048
Jason Horvath <kaingnx@gmail.com>
2022-07-18 12:35:13 -0400

    remove console

--- src/core/RouteCollection.ts
+++ src/core/RouteCollection.ts
@@ -14,22 +14,18 @@ export default class RouteCollection {
 
   get(uri: string): RouteEntry {
     const matchedPath = this.matchRoutePath(uri);
-    console.log(matchedPath);
-    console.log(this.routeMap.get(matchedPath));
     return new RouteEntry(matchedPath, this.routeMap.get(matchedPath));
   }
 
   matchRoutePath(uri: string): String {
     const matches = [...this.routeMap.keys()].filter((item: String) => {
       const uriPattern = item.replace(/:[a-zA-Z0-9]+/g, '[a-zA-Z0-9]+');
-      console.log(uriPattern);
       const regex = new RegExp(uriPattern);
 
       return regex.test(uri) && (uri.split('/').length === item.split('/').length);
     });
-    console.log(matches);
+
     const match = matches.includes(uri) ? uri : matches.pop() ?? '/404';
-    console.log(match);
     return match;
   }
 
