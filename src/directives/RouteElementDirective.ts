0b2fe6ea4da80dcc858ed03f89c198800110b22e
Jason Horvath <kaingnx@gmail.com>
2022-07-19 08:10:41 -0400

    add docblock

--- src/directives/RouteElementDirective.ts
+++ src/directives/RouteElementDirective.ts
@@ -2,6 +2,11 @@ import {html, unsafeStatic} from 'lit/static-html.js';
 import { Directive, directive } from 'lit/directive.js';
 import RenderProps from '../core/RenderProps';
 
+/**
+ * Used for dynamically rendering tags into the shadow dom.
+ * 
+ * Works in conjunction with the RouterOutlet
+ */
 class RouteElementDirective extends Directive {
   render(tag: string, props: RenderProps) {
     return html`<${unsafeStatic(tag)} .props=${props}></${unsafeStatic(tag)}>`;
