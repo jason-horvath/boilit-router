6e1ecf35d5533f4eff0f900995c20f4334f507c1
Jason Horvath <kaingnx@gmail.com>
2022-07-19 08:11:50 -0400

    add comment

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -1,6 +1,10 @@
 import RouteCollection from '../core/RouteCollection';
 import Route from '../core/Route';
 
+/**
+ * This is a set of base routes for example. It is best to rely on a new RouteCollection as seen below in your own routes file.
+ */
+
 const baseRoutes = new RouteCollection();
 
 baseRoutes.add('/', new Route('default-index-view', require('../views/DefaultIndexView')));
