ea75f61a34fcd9dc1cc7e15aff41b86245adf6c9
Jason Horvath <kaingnx@gmail.com>
2022-07-23 15:18:19 -0400

    improve route matching for proper 404

--- src/core/RouteCollection.ts
+++ src/core/RouteCollection.ts
@@ -3,6 +3,13 @@ import RouteEntry from './RouteEntry';
 export default class RouteCollection {
 
   /**
+   * Not Found Uri
+   * 
+   * @var notFoundUri
+   */
+  private notFoundUri: string = '/404';
+
+  /**
    * This should be where all routes are stored.
    * 
    * @var routeMap A RouteCollection of all Route from the configured routes
@@ -31,31 +38,47 @@ export default class RouteCollection {
   /**
    * Get a RouteEntry from the collection.
    * 
-   * @param uri The uri returned from the browser, not a route path.
+   * @param path The uri returned from the browser, not a route path.
    * @returns RouteEntry that is part of this.routeMap
    */
-  get(uri: string): RouteEntry {
-    const matchedPath = this.matchRoutePath(uri);
+  get(path: string): RouteEntry {
+    const matchedPath = this.matchRoutePath(path);
     return new RouteEntry(matchedPath, this.routeMap.get(matchedPath));
   }
 
   /**
    * Match what is entered in then window.location.pathname to an entry in the RouteCollecton
    * 
-   * @param uri The uri that is from the browser, not a route path
+   * For dynamic URLs the return is early if there was a matchign dynamic path, the static routes, are checked to make sure they match exactly
+   * so that something like `/about` does not come back as a match for  `/aboutExtrachars`. Mis matched dynamic route will ultimate fail at the point of regex checks
+   * 
+   * @param path The uri that is from the browser, not a route path
    * @returns The matched route path/key that may be found in the this.routeMap
    * 
    */
-  matchRoutePath(uri: string): String {
+  matchRoutePath(path: string): String {
     const matches = [...this.routeMap.keys()].filter((item: String) => {
-      const uriPattern = item.replace(/:[a-zA-Z0-9]+/g, '[a-zA-Z0-9]+');
-      const regex = new RegExp(uriPattern);
+      const regex = new RegExp(this.uriMatchPattern(item));
 
-      return regex.test(uri) && (uri.split('/').length === item.split('/').length);
+      return regex.test(path) && (path.split('/').length === item.split('/').length);
     });
 
-    const match = matches.includes(uri) ? uri : matches.pop() ?? '/404';
-    return match;
+    const match = matches.pop() ?? '';
+
+    if(match.indexOf(':') !== -1) {
+      return match;
+    }
+    
+    return match === path ? match : '';
+  }
+
+  /**
+   * Uri Match Pattern
+   * 
+   * @return string Return the route path with regex in it
+   */
+   uriMatchPattern(source: String): string {
+    return source.replace(/:[a-zA-Z0-9]+/g, '[a-zA-Z0-9]+');
   }
 
   /**
@@ -74,5 +97,14 @@ export default class RouteCollection {
    */
   getRouteMap(): Map<String, Route> {
     return this.routeMap;
+  }
+
+  /**
+   * Get the no found Uri for external use.
+   * 
+   * @returns this.notDoundUri
+   */
+  getNotFoundUri(): string {
+    return this.notFoundUri;
   }
 }
